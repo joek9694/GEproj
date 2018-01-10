7e64b7ac6f66e7f7a2eb5586ec884626be9f59a2
Johan <Johan.Eklundh@gmail.com>
2018-01-10 01:50:48 +0100

    Add project files.

---
+++ GEproj/Main.cpp
@@ -0,0 +1,15 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include<SDL_mixer.h>
+
+class GameEngine {
+public:
+
+private:
+};
+
+int main(int argc, char** argv) {
+
+	return 0;
+};
